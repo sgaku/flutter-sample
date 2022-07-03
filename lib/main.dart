2c12d3bbfd88e383aaffaa9f83490cd5f57fe1b9
Gaku Sato <ggg514@icloud.com>
2022-07-03 17:06:25 +0900

    変数の操作

--- lib/main.dart
+++ lib/main.dart
@@ -15,14 +15,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body:  Center(
+          ///Stringに変更
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
